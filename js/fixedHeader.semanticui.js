1343003b93764d55240bbddb68a199d0b544902a
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-07 16:17:51 +0000

    43cfde4f36a20955a840ca5301328839c2337bff Release 3.3.1: Build error so releasing
    Sync to source repo @43cfde4f36a20955a840ca5301328839c2337bff

--- js/fixedHeader.semanticui.js
+++ js/fixedHeader.semanticui.js
@@ -1,4 +1,3 @@
-
 /*! Semanic UI styling wrapper for FixedHeader
  * © SpryMedia Ltd - datatables.net/license
  */
--- js/fixedHeader.semanticui.mjs
+++ js/fixedHeader.semanticui.mjs
@@ -1,11 +1,10 @@
-
 /*! Semanic UI styling wrapper for FixedHeader
  * © SpryMedia Ltd - datatables.net/license
  */
 
 import $ from 'jquery';
 import DataTable from 'datatables.net-se';
-import DataTable from 'datatables.net-fixedheader';
+import 'datatables.net-fixedheader';
 
 
 
