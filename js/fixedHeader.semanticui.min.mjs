7f98c89486867b0086224a0c0d9d7257ad46377f
dtbuild <dtbuild@rigil>
2026-05-07 13:47:38 +0000

    7cf0b29dd3d805e373e6cd1aad5efbc6fffbf1dd
    Sync to source repo @bb639877e328dd51de2a396ecc51e574e100846c

--- js/fixedHeader.semanticui.min.mjs
+++ js/fixedHeader.semanticui.min.mjs
@@ -1,4 +1,4 @@
-/*! Semanic UI styling wrapper for FixedHeader
- * © SpryMedia Ltd - datatables.net/license
+/*! FixedHeader Fomantic styling 5.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
-import jQuery from"jquery";import DataTable from"datatables.net-se";import FixedHeader from"datatables.net-fixedheader";let $=jQuery;export default DataTable;
+import DataTable from"datatables.net-se";import FixedHeader from"datatables.net-fixedheader";export default DataTable;
--- js/fixedHeader.semanticui.mjs
+++ js/fixedHeader.semanticui.mjs
@@ -1,14 +1,11 @@
-/*! Semanic UI styling wrapper for FixedHeader
- * © SpryMedia Ltd - datatables.net/license
+/*! FixedHeader Fomantic styling 5.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
-import jQuery from 'jquery';
 import DataTable from 'datatables.net-se';
 import FixedHeader from 'datatables.net-fixedheader';
-
-// Allow reassignment of the $ variable
-let $ = jQuery;
 
 
 
 export default DataTable;
+
